37a18f5ff60e288cdf3d364b51c2b944c65742dc
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-12 12:43:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-export function allClass(clas, change){
+function allClass(clas, change){
     let x = document.getElementsByClassName(clas)
     for (let i = 0 ; i < x.length ; i++){
             x[i].innerHTML = change

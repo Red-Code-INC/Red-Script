67ddebe54330330b1ddc85cd9cb6a5c21eb1f839
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-13 11:12:43 -0500

    Create search.js

---
+++ search.js
@@ -0,0 +1,14 @@
+function search_website(search) {
+    let input = document.getElementById(search).value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName(search);
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}

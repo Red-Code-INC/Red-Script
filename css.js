8b4a465dc425796ae3e99d52ff900beb654a1904
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-13 07:55:49 -0500

    Update css.js

--- css.js
+++ css.js
@@ -1,1 +1,11 @@
+var head = document.getElementsByTagName('HEAD')[0];
+ 
+var link = document.createElement('link');
+ 
+link.rel = 'stylesheet';
+     
+link.type = 'text/css';
+     
+link.href = 'red.css';
 
+head.appendChild(link);

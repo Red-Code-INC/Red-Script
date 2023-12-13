cf408e2a3d8dfe889e2a0788be164288e9c01293
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-13 07:52:24 -0500

    Delete time/shortcuts directory

--- time/shortcuts/red.js
+++
@@ -1,8 +0,0 @@
-function red(){
-    red = document.getElementsByTagName("html")
-    for (let i = 0 ; i < red.length ; i++){
-    red[i].style.color = "darkred"
-    red[i].style.backgroundColor = "red"
-    }
-}
-window.red = red

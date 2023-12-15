49b917fbf88a9d1bed3fa0f52ca58c645e50a3aa
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-15 07:52:53 -0500

    Create timer.js

---
+++ time/timer.js
@@ -0,0 +1,20 @@
+function clock(timeset, location) {
+  var myTimer = setInterval(myClock, 1000);
+  var c = (timeset); //Initially set to 1 hour
+
+
+  function myClock() {
+    
+    var seconds = c % 60; // Seconds that cannot be written in minutes
+    var minutes = (c - seconds) / 60; // Gives the seconds that COULD be given in minutes
+    var minutesLeft = minutes % 60; // Minutes that cannot be written in hours
+    var hours = (minutes - minutesLeft) % 60;
+    // Now in hours, minutes and seconds, you have the time you need.
+    
+    document.getElementById(location).innerHTML =hours + ":" + minutes + ":" + seconds
+    if (c == 0) {
+      clearInterval(myTimer);
+    }
+  }
+}
+window.clock = clock

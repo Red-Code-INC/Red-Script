5094e26cdee9455accb29034d60e0ce48b683a9a
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-15 11:02:01 -0500

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,4 +1,4 @@
-let dtab = document.getElementsByName("defaultOpen")
+let dtab = document.getElementById("defaultOpen")
 function tab(evt, tabName, tabcontents, tablink,) {
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -20,11 +20,6 @@ function tab(evt, tabName, tabcontents, tablink,) {
   evt.currentTarget.className += " active";
 }
 // Get the element with id="defaultOpen" and click on it
-window.addEventLisetner("onload", "loaddefault")
-function loaddefault(){
-  for (let i = 0 ; i < dtab.length ; i++){
-    dtab[i].click();
-  }
-}
+  dtab.click();
 
 window.tab = tab

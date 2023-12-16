7e5cf07e2a24235aef7e1f0b8bbfcfcec54abca8
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-16 12:26:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ let dfault = document.getElementsByName('defaultOpen')
 for (let i = 0 ; i < dfault.length ; i++){
 dfault[i].click();
 }
+var buttons = document.getElementsByClassName("button");
+var activeSheet = document.getElementById("active-stylesheet");
+var clearStorageButton = document.getElementById("clear-storage");
+
+// Test to see if localStorage already has a value stored
+if (localStorage.getItem("lastActiveSheet")) {
+     activeSheet.setAttribute("href", localStorage.getItem("lastActiveSheet"));
+} else {
+    activeSheet.setAttribute("href", "https://cdn.jsdelivr.net/gh/Sideswipe5/Red-CSS@1.0.7/themes/main/default.css")
+} 
+
+// Assign the event lister to each button
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", switchStyle);  
+}
+// Create a button to clear localStorage
+clearStorageButton.addEventListener("click", clearStorage);
+
+// Set the #active-stylesheet to be the light or dark stylesheet
+function switchStyle() {
+   var selectedSheet = this.getAttribute("data-stylesheet");
+   activeSheet.setAttribute("href", selectedSheet);
+   localStorage.setItem("lastActiveSheet", selectedSheet);
+}
+
+// Wrapper function to localStorage.clear
+function clearStorage() {
+  localStorage.clear();
+}

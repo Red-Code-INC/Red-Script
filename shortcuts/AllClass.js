7dd86736249cfba7d82601414efc3a52722a49a1
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-17 09:37:52 -0500

    Update AllClass.js

--- shortcuts/AllClass.js
+++ shortcuts/AllClass.js
@@ -1,7 +1,27 @@
-function allClass(clas, change){
+function allClass(clas,effect ,change){
     let x = document.getElementsByClassName(clas)
     for (let i = 0 ; i < x.length ; i++){
+        if (effect == "innerHTML"){
             x[i].innerHTML = change
+        } else if (effect == "appendChild"){
+        x[i].appendChild(change)
+        } else if (effect == "display"){
+            x[i].style.display = change
+        } else if (effect == "color"){
+            x[i].style.color = change
+        } else if (effect == "backgroundColor"){
+            x[i].style.backgroundColor = change
+        } else if (effect == "fontSize"){
+            x[i].style.fontSize = change
+        } else if (effect == "href"){
+            x[i].setAttribute("href", change)
+        } else if (effect == "append"){
+            x[i].append(change)
+        } else if (effect == "innerText"){
+            x[i].innerText = change
+        } else if (effect == "fontFamily"){
+            x[i].style.fontFamily = change
+        }
     }
 }
 window.allClass = allClass

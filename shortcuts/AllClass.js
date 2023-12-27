59a7bb52fd0ff0d8bd2d3bd0541c8905206e3b9e
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2023-12-27 11:21:04 -0500

    Update AllClass.js

--- shortcuts/AllClass.js
+++ shortcuts/AllClass.js
@@ -1,27 +1,31 @@
-function allClass(clas,effect ,change){
+function allClass(clas,mthod,change){
     let x = document.getElementsByClassName(clas)
     for (let i = 0 ; i < x.length ; i++){
-        if (effect == "innerHTML"){
+        if (mthod == "innerHTML"){
             x[i].innerHTML = change
-        } else if (effect == "appendChild"){
+        } else if (mthod == "appendChild"){
         x[i].appendChild(change)
-        } else if (effect == "display"){
+        } else if (mthod == "display"){
             x[i].style.display = change
-        } else if (effect == "color"){
+        } else if (mthod == "color"){
             x[i].style.color = change
-        } else if (effect == "backgroundColor"){
+        } else if (mthod == "backgroundColor"){
             x[i].style.backgroundColor = change
-        } else if (effect == "fontSize"){
+        } else if (mthod == "fontSize"){
             x[i].style.fontSize = change
-        } else if (effect == "href"){
+        } else if (mthod == "href"){
             x[i].setAttribute("href", change)
-        } else if (effect == "append"){
+        } else if (mthod == "append"){
             x[i].append(change)
-        } else if (effect == "innerText"){
+        } else if (mthod == "innerText"){
             x[i].innerText = change
-        } else if (effect == "fontFamily"){
+        } else if (mthod == "fontFamily"){
             x[i].style.fontFamily = change
+        } else if (mthod == "fontWeight"){
+            x[i].style.fontWeight = change
         }
     }
 }
-window.allClass = allClass
+let $C =  allClass
+
+window.$C = $C

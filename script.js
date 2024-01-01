621015f26e8291123931ea8ce310ba5f8f6ccc12
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-01-01 12:25:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import "./css.js"
 import "./red.js"
 import "./shortcuts/AllClass.js"
-import "./time/RelativeScript.js"
+import "./time/DateTag.js"
 import "./time/Formatsetdate.js"
 import "./shortcuts/AllTag.js"
 import "./shortcuts/AllName.js"

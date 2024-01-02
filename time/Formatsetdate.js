147a6f8367e733b9c8f08d7a676435bbab9095a7
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-01-02 15:23:16 -0500

    Update Formatsetdate.js

--- time/Formatsetdate.js
+++ time/Formatsetdate.js
@@ -1,43 +1,21 @@
-function formatSetdate(timestated){
-const date = new Date(timestated)
-
+function formatSetDate(timestated){
+const dt = new Date(timestated);
 let dates = document.getElementsByClassName(timestated)
+const padL = (nr, len = 2, chr = `0`) => `${nr}`.padStart(2, chr);
+let time =(`${
+    padL(dt.getMonth()+1)}/${
+    padL(dt.getDate())}/${
+    dt.getFullYear()} ${
+    padL(dt.getHours())}:${
+    padL(dt.getMinutes())}:${
+    padL(dt.getSeconds())} ${
+    padL(dt.getHours() >= 12 ? 'am' : 'pm')}`
+);
 
-const formatData = (input) => { 
-  if (input > 9) { 
-    return input; 
-  } else return `0${input}`; 
-}; 
-
-// Function to convert 
-// 24 Hour to 12 Hour clock 
-const formatHour = (input) => { 
-  if (input > 12) { 
-    return input - 12; 
-  }
-  return input; 
-}; 
-
-// Data about date 
-const format = { 
-  dd: formatData(date.getDate()), 
-  mm: formatData(date.getMonth() + 1), 
-  yyyy: date.getFullYear(), 
-  HH: formatData(date.getHours()), 
-  hh: formatData(formatHour(date.getHours())), 
-  MM: formatData(date.getMinutes()), 
-  SS: formatData(date.getSeconds()), 
-};
-let hours = date.getHours()
-let ampm = hours >= 12 ? 'pm' : 'am' 
-const format12Hour = ({ dd, mm, yyyy, hh, MM, SS,}) => { 
-   out = (`${mm}/${dd}/${yyyy} ${hh}:${MM}:${SS}${ampm}`); 
-}; 
-// Time in 12 Hour format 
-format12Hour(format);
- 
 for (let i = 0 ; i < dates.length ; i++){
-    dates[i].innerHTML = out
+    dates[i].innerHTML = time
 }
 }
-window.formatSetdate = formatSetdate
+let $D = formatSetDate
+
+window.$D = $D

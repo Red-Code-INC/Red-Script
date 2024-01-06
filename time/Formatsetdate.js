05250b53ef10c7097dc2bfadceadd5711feef684
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-01-06 13:31:32 -0500

    Update Formatsetdate.js

--- time/Formatsetdate.js
+++ time/Formatsetdate.js
@@ -4,27 +4,31 @@ let dtes =  "[time= " + "'" + timestated + "'" +"]"
     let dates = document.querySelectorAll(dtes)
 const padL = (nr, len = 2, chr = `0`) => `${nr}`.padStart(2, chr);
 let hours = dt.getHours()
+let day = dt.getDate()
+let month = dt.getMonth()+1
+let year = dt.getFullYear()
 let ampm = ""
 if (hours > 12){
     hours = hours -= 12
      ampm = 'pm'
-} else {
+} else if (hours > 11 && hours < 13){
+    ampm = 'pm'
+} else if (hours < 12){
     ampm = 'am'
 }
 if (hours < 1){
     hours = hours += 12
 }
 
-
 let time =(`${
-    padL(dt.getMonth()+1)}/${
-    padL(dt.getDate())}/${
-    dt.getFullYear()} ${
+    padL(month)}/${
+    padL(day)}/${
+    padL(year)} ${
     padL(hours)}:${
     padL(dt.getMinutes())}:${
     padL(dt.getSeconds())} ${
     padL(ampm)}`
-);
+); 
 
 for (let i = 0 ; i < dates.length ; i++){
     dates[i].innerHTML = time
@@ -33,4 +37,3 @@ for (let i = 0 ; i < dates.length ; i++){
 let $D = formatSetDate
 
 window.$D = $D
-

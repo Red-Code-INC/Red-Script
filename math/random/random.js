20b58e5a93f4e6c15a6a612952f975f6dbf88b34
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-01-30 13:43:44 -0500

    Update random.js

--- math/random/random.js
+++ math/random/random.js
@@ -1,1 +1,10 @@
-
+function $round(x){
+    return Math.round(x)
+}
+function $RD(value){
+    if (value == undefined){
+        return $round(Math.random())
+    } else {
+        return $round(Math.random()*value)
+    }
+}

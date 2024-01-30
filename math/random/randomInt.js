d2dfcd7bbd97943718f83ff2e98e976f3b549d96
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-01-30 13:46:18 -0500

    Update randomInt.js

--- math/random/randomInt.js
+++ math/random/randomInt.js
@@ -1,8 +1,5 @@
 import "./random.js"
 
-function randomInt(min,max){
-    return $round(Math.random()*(max - min) + min)
+function $RND(min,max){
+    return $RD(((max - min)+min))
 }
-let $RDI = randomInt
-
-window.$RDI = $RDI

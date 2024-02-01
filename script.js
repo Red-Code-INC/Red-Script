cbfac20427ff6c7253ccd532dcdc489860b4e1bf
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-02-01 11:03:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ let dfault = document.getElementsByName('defaultOpen')
 for (let i = 0 ; i < dfault.length ; i++){
 dfault[i].click();
 }
+var acc = document.getElementsByClassName('accordion');
+var i;
+window.dropdownfunc = dropdownfunc
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function(){
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  }
+  );
+}

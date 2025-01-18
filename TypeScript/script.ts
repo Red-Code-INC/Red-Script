cd2cb74cd02aee892135e317e5f15a9e260f9abb
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2025-01-17 22:19:50 -0500

    Create script.ts

---
+++ TypeScript/script.ts
@@ -0,0 +1,11 @@
+import "./script.js";
+
+type ts = string
+type ti = number
+type tb = boolean
+type ta = any 
+type tn = null
+type tv = void
+type tu = undefined
+type tk = unknown
+type tnv = never
